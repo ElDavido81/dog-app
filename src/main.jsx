ac900d23525658ea6a4fbb54080a354fc1f61957
ElDavido81 <eldavido81@gmail.com>
2025-09-22 12:42:27 +0200

    css removed

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 import { createHashRouter, RouterProvider } from 'react-router'
 import Catalog from './pages/Catalog.jsx'
@@ -24,6 +23,5 @@ const router = createHashRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 <RouterProvider router={router} />
-
   </StrictMode>,
 )
